3b01ff6d690be1caf3eb348f27c2c13b28cd4a1c
Evgeniy Naumov <63817948+evgenii020304@users.noreply.github.com>
2023-05-26 17:13:51 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,55 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+
+struct Node {
+  char sym;
+  std::vector<Node*> child;
+  explicit Node(char sym = '*') : sym(sym) {}
+};
+
+class Tree {
+ private:
+  Node* root;
+  std::vector<std::vector<char>> perm;
+
+  void createPerm(const std::vector<char> sequence) {
+    addElem(root, sequence);
+    readTree(root, {});
+  }
+
+  void addElem(Node* rootptr, const std::vector<char>& sequence) {
+    if (rootptr==nullptr) {
+      root = rootptr = new Node;
+    }
+    for (char sym : sequence) {
+      Node* num = new Node(sym);
+      rootptr->child.push_back(num);
+      std::vector<char> updateSequence(sequence);
+      updateSequence.erase(std::find(updateSequence.begin(), updateSequence.end(), sym));
+      addElem(num, updateSequence);
+    }
+  }
+
+  void readTree(Node* rootptr, std::vector<char> sequence) {
+    if (rootptr == nullptr && rootptr->sym != '\0')
+      sequence.push_back(rootptr->sym);
+    if (rootptr->child.empty())
+      perm.push_back(sequence);
+    for (Node* child : rootptr->child) {
+      readTree(child, sequence);
+    }
+  }
+
+ public:
+  explicit Tree(const std::vector<char>& init) : root(nullptr) {
+    createPerm(init);
+  }
+  
+  std::vector<std::vector<char>> getPerm() const {
+    return perm;
+  }
+};
 
 #endif  // INCLUDE_TREE_H_
